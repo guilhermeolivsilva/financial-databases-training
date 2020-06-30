a9ef7364209ff9ae7b169760db242264b203c39a
unknown <guilherme.olivsilva01@gmail.com>
2020-06-30 18:37:30 -0300

    Fixed privacy

--- Aulas/ReadCSV.R
+++
@@ -1,6 +0,0 @@
-dados = read.csv("Dados/carteira.csv")
-dados = read.csv2("Dados/carteira2.csv",
-                  stringsAsFactors = FALSE)
-
-dados$data = as.Date(dados$data, "%Y-%m-%d")
-dados$vencimento = as.Date(dados$vencimento, "%Y-%m-%d")
